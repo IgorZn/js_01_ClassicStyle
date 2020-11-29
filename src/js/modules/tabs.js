d8f4f68726a9f3ed742a4790266d8e99bfe791cf
Igor Znamensky <igor.znamensky@gmail.com>
2020-11-29 18:58:26 +0300

    01 Проект №1. Классический стиль 004 Работа с табами (вкладками) на странице, 6-11

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,9 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent(){
+
+    }
+}
