4244537e09aec43efac51e7e5cc89dfe45ba2715
Igor Znamensky <igor.znamensky@gmail.com>
2020-12-08 10:48:27 +0300

    005 Работа с формами отправки данных ...... 006 Работа с формой-калькулятором, часть 1 Next: 007 Работа с формой-калькулятором, часть 2

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,8 +1,9 @@
 const modals = () => {
-    function bindModals(triggerSelector, modalSelector, closeSelector) {
+    function bindModals(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            close = document.querySelector(closeSelector);
+            close = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]')
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) =>{
@@ -10,23 +11,37 @@ const modals = () => {
                     e.preventDefault();
                 }
 
+                windows.forEach(item =>{
+                   item.style.display = 'none'
+                });
+
                 modal.style.display = "block";
-                // document.body.style.overflow = "hidden";
-                document.body.classList.add('modal-open');
+                document.body.style.overflow = "hidden";
+                // document.body.classList.add('modal-open');
             });
         });
 
         close.addEventListener('click', () =>{
+
+             windows.forEach(item =>{
+                   item.style.display = 'none'
+                });
+
             modal.style.display = "none";
             document.body.style.overflow = "";
             // document.body.classList.remove('modal-open');
         });
 
         modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
+
+                windows.forEach(item =>{
+                   item.style.display = 'none'
+                });
+
                  modal.style.display = "none";
-                // document.body.style.overflow = "";
-                document.body.classList.remove('modal-open');
+                document.body.style.overflow = "";
+                // document.body.classList.remove('modal-open');
             };
         });
     }
@@ -40,8 +55,11 @@ const modals = () => {
 
 
 
-    bindModals('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModals('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close', false);
     bindModals('.phone_link', '.popup', '.popup_close');
+    bindModals('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModals('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+    bindModals('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
     // showModalByTime('.popup', 6000)
 };
 
