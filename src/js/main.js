ce0202fb3ed0cff741ae20d56909d343e3cffe84
Igor Znamensky <igor.znamensky@gmail.com>
2020-12-18 11:26:43 +0300

    007 Работа с формой-калькулятором, часть 2 (ДЗ сделаны) 008 Работа с таймером ...... Next: 009 Реализуем модуль с показом изображений

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict"
@@ -11,6 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // что-то выбирает, а мы записываем это в словарь
     // и в конце получаем словарь со всеми атрибутами
     let modalState = {};
+    let deadline = '2020-12-31'
 
     changeModalState(modalState);
     modals();
@@ -18,4 +20,5 @@ window.addEventListener('DOMContentLoaded', () => {
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
     forms(modalState);
+    timer('.container1', deadline);
 })
